93f7d30bed50ecdff194517af0b08435d0f57e2d
Alexander Yakovlev <job.yakovlev.alexander@gmail.com>
2017-12-24 16:33:01 +0700

    refactor makeAst

--- src/index.js
+++ src/index.js
@@ -10,71 +10,49 @@ const readConfigFile = (pathToFile) => {
   return parseConfigFile(extention, dataFile);
 };
 
-const isaddedProperty = (objBefore, objAfter, property) =>
-  !(property in objBefore);
-
-const isEqualProperty = (objBefore, objAfter, property) =>
-  _.isEqual(objBefore[property], objAfter[property]);
-
-const isModfifiedProperty = (objBefore, objAfter, property) => {
-  if (property in objBefore && property in objAfter) {
-    return !_.isEqual(objBefore[property], objAfter[property]);
-  }
-  return false;
-};
-
-const isDeletedProperty = (objBefore, objAfter, property) =>
-  !(property in objAfter);
-
 const makeAst = (objBefore, objAfter) => {
   const properties = _.union(Object.keys(objBefore), Object.keys(objAfter));
   return properties.map((property) => {
-    if (isaddedProperty(objBefore, objAfter, property)) {
+    if (!(property in objBefore)) {
       return {
         property,
         type: 'added',
         valueAfter: objAfter[property],
       };
     }
-    if (isDeletedProperty(objBefore, objAfter, property)) {
+    if (!(property in objAfter)) {
       return {
         property,
         type: 'deleted',
         valueBefore: objBefore[property],
       };
     }
-    if (isEqualProperty(objBefore, objAfter, property)) {
+    if (_.isEqual(objBefore[property], objAfter[property])) {
       return {
         property,
         type: 'equal',
         valueBefore: objBefore[property],
       };
     }
-    if (isModfifiedProperty(objBefore, objAfter, property)) {
-      const isTowObjects = _.isObject(objBefore[property]) && _.isObject(objAfter[property]);
-      if (isTowObjects) {
-        const obj1 = objBefore[property];
-        const obj2 = objAfter[property];
-        return {
-          property,
-          type: 'nested',
-          children: makeAst(obj1, obj2),
-        };
-      }
+    if (_.isObject(objBefore[property]) && _.isObject(objAfter[property])) {
       return {
         property,
-        type: 'modified',
-        valueBefore: objBefore[property],
-        valueAfter: objAfter[property],
+        type: 'nested',
+        children: makeAst(objBefore[property], objAfter[property]),
       };
     }
-    return {};
+    return {
+      property,
+      type: 'modified',
+      valueBefore: objBefore[property],
+      valueAfter: objAfter[property],
+    };
   });
 };
 
-const makeDiff = (valueBefore, valueAfter, format = 'complex') => {
-  const objBefore = readConfigFile(valueBefore);
-  const objAfter = readConfigFile(valueAfter);
+const makeDiff = (configFileBefore, configFileAfter, format = 'complex') => {
+  const objBefore = readConfigFile(configFileBefore);
+  const objAfter = readConfigFile(configFileAfter);
   const render = getRender(format);
   return render(makeAst(objBefore, objAfter));
 };
