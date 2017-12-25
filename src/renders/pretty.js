0e770767c25c857bde8d4e7a4a6ec17ec7c75fd3
Alexander Yakovlev <job.yakovlev.alexander@gmail.com>
2017-12-25 11:27:30 +0700

    fix pretty render

--- src/renders/pretty.js
+++ src/renders/pretty.js
@@ -8,35 +8,30 @@ const endBlock = '}';
 const makeRow = (property, value, sign, level) => `${intend.repeat(level)}${sign} ${property}: ${value}${eol}`;
 
 const toString = (obj, level) => {
-  if (!_.isObject(obj)) {
-    return obj;
+  if (_.isObject(obj)) {
+    const properties = Object.keys(obj);
+    const outRow = properties.reduce((acc, element) => {
+      const row = makeRow(element, toString(obj[element], level + 1), ' ', level + 2);
+      return `${acc}${row}`;
+    }, '');
+    return `${startBlock}${eol}${outRow}${intend.repeat(level + 1)}${endBlock}`;
   }
-  const properties = Object.keys(obj);
-  const outRow = properties.reduce((acc, element) => {
-    const row = makeRow(element, toString(obj[element], level + 1), ' ', level + 2);
-    return `${acc}${row}`;
-  }, '');
-  return `${startBlock}${eol}${outRow}${intend.repeat(level + 1)}${endBlock}`;
-};
-
-const typeMapping = {
-  added: (element, level) => makeRow(element.property, toString(element.valueAfter, level), '+', level),
-  deleted: (element, level) => makeRow(element.property, toString(element.valueBefore, level), '-', level),
-  equal: (element, level) => makeRow(element.property, toString(element.valueBefore, level), ' ', level),
-  nested: (element, level, value) => makeRow(element.property, value, ' ', level),
-  modified: (element, level) => {
-    const rowAfter = makeRow(element.property, toString(element.valueAfter, level), '+', level);
-    const rowBefore = makeRow(element.property, toString(element.valueBefore, level), '-', level);
-    return `${rowAfter}${rowBefore}`;
-  },
+  return obj;
 };
 
 const astToString = (ast, level = 1) => {
-  const outString = ast.reduce((acc, element) => {
-    const value = element.children ? astToString(element.children, level + 2) : '';
-    const astRow = typeMapping[element.type](element, level, value);
-    return `${acc}${astRow}`;
-  }, '');
+  const typeMapping = {
+    added: element => makeRow(element.property, toString(element.valueAfter, level), '+', level),
+    deleted: element => makeRow(element.property, toString(element.valueBefore, level), '-', level),
+    equal: element => makeRow(element.property, toString(element.valueBefore, level), ' ', level),
+    nested: element => makeRow(element.property, astToString(element.children, level + 2), ' ', level),
+    modified: (element) => {
+      const rowAfter = makeRow(element.property, toString(element.valueAfter, level), '+', level);
+      const rowBefore = makeRow(element.property, toString(element.valueBefore, level), '-', level);
+      return `${rowAfter}${rowBefore}`;
+    },
+  };
+  const outString = ast.map(element => typeMapping[element.type](element, level)).join('');
   return `${startBlock}${eol}${outString}${intend.repeat(level - 1)}${endBlock}`;
 };
 
