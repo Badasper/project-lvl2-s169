bd39241ab19ac96aead53cb4c1fd67b1a1722924
Alexander Yakovlev <job.yakovlev.alexander@gmail.com>
2017-12-25 12:22:30 +0700

    fix pretty render

--- src/renders/pretty.js
+++ src/renders/pretty.js
@@ -9,9 +9,8 @@ const makeRow = (property, value, sign, level) => `${intend.repeat(level)}${sign
 
 const toString = (obj, level) => {
   if (_.isObject(obj)) {
-    const objStr = Object.keys(obj).map((element) =>
-      `${makeRow(element, toString(obj[element], level + 1), ' ', level + 2)}`).join('');
-    return `${startBlock}${eol}${objStr}${intend.repeat(level + 1)}${endBlock}`;
+    const objStr = Object.keys(obj).map(element => `${makeRow(element, toString(obj[element], level + 1), ' ', level + 2)}`);
+    return `${startBlock}${eol}${objStr.join('')}${intend.repeat(level + 1)}${endBlock}`;
   }
   return obj;
 };
@@ -22,8 +21,8 @@ const astToString = (ast, level = 1) => {
     deleted: element => makeRow(element.property, toString(element.valueBefore, level), '-', level),
     equal: element => makeRow(element.property, toString(element.valueBefore, level), ' ', level),
     nested: element => makeRow(element.property, astToString(element.children, level + 2), ' ', level),
-    modified: (element) => `${makeRow(element.property, toString(element.valueAfter, level),
-      '+', level)}${makeRow(element.property, toString(element.valueBefore, level), '-', level)}`,
+    modified: element => `${makeRow(element.property, toString(element.valueAfter, level), '+', level)
+    }${makeRow(element.property, toString(element.valueBefore, level), '-', level)}`,
   };
   const outString = ast.map(element => typeMapping[element.type](element, level)).join('');
   return `${startBlock}${eol}${outString}${intend.repeat(level - 1)}${endBlock}`;
